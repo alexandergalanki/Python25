052dc139d3c3b00f306177525b9a1b0cec5c3170
Alexander Galanki <galankialex@gmail.com>
2025-01-27 16:32:45 +0530

    Add files via upload

---
+++ SubsTemplateliterals.js
@@ -0,0 +1,7 @@
+//Substitution String
+let count =5;
+console.log("there are %d types of fruits in the basket",count);
+
+//Template literals
+let count2=10;
+console.log(`there are ${count2} types of fruits in the basket`);
