2b3b718a204fa2c042f4fccf112b5940b36c5932
Alexander Galanki <galankialex@gmail.com>
2025-01-28 14:40:50 +0530

    Add files via upload

---
+++ PracticeSet.js
@@ -0,0 +1,22 @@
+let a="Bobby";
+let b=5;
+console.log(a+b);
+console.log(typeof a);
+
+const k={
+    name:"Bobby",
+    age:26
+}
+k["isTeacher"]=true;
+
+console.log(k["isTeacher"]);
+
+const dict={
+    "appreciate":"to enjoy something or to understand the value of somebody/something",
+    "boycott":"to refuse to take part in an event, buy things from a particular company, etc. because you strongly disapprove of it",
+    "Ataratia":"distresss",
+    "yak":"an animal of the cow family, with long horns and long hair, that lives in central Asia",
+    "divulgate":"to make publicly known"
+}
+
+console.log(dict["appreciate"]);
